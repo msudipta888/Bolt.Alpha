6662196406a2a8f07a46ec90a45210deb5496bb6
Sudipta Mondal <msudipta857@gmail.com>
2025-03-26 22:56:03 +0530

    resolve all issue

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
-// next.config.js
-module.exports = {
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-        ],
-      },
-    ];
-  },
-};
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  /* config options here */
+}
+ 
+export default nextConfig
